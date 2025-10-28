203f2324c03e768a413222fe43dfe6d6cbaf3ff8
Wo0zZ1 <Mper06@mail.ru>
2025-10-29 00:56:50 +0300

    0.1.13

--- src/responses/link.responses.ts
+++ src/responses/link.responses.ts
@@ -1,12 +1,7 @@
 import { ILink, ILinkStats } from '../entities'
 
-// ============================================
-// Link Responses (in method order)
-// ============================================
-
 /**
  * Response for GET /links/redirect/:shortLink
- * Public endpoint for redirecting short links
  */
 export interface RedirectResponse {
 	url: string
@@ -14,7 +9,6 @@ export interface RedirectResponse {
 
 /**
  * Response for GET /links/user/:userId
- * Get user links with pagination
  */
 export interface GetUserLinksResponse {
 	links: ILink[]
@@ -28,48 +22,33 @@ export interface GetUserLinksResponse {
 
 /**
  * Response for POST /links/user/:userId
- * Create a new link
  */
 export type CreateLinkResponse = ILink
 
 /**
  * Response for GET /links/id/:shortLink
- * Get link information by short link
  */
 export type GetLinkByShortLinkResponse = ILink
 
 /**
  * Response for DELETE /links/:shortLink
- * Delete a link
  */
 export type DeleteLinkResponse = ILink
 
 /**
  * Response for GET /links/:shortLink/stats
- * Get link statistics
  */
 export type GetLinkStatsResponse = ILinkStats
 
 /**
  * Response for GET /links/:shortLink/qr
- * Generate QR code (returns Buffer)
  */
 export type GetQRCodeResponse = Buffer
 
 /**
  * Response for GET /links/user/:userId/stats
- * Get user links statistics
  */
 export interface GetUserLinksStatsResponse {
 	totalLinks: number
 	totalRedirects: number
 }
-
-// ============================================
-// Aliases for backward compatibility
-// ============================================
-
-export type PaginatedLinksResponse = GetUserLinksResponse
-export type PaginatedLinks = GetUserLinksResponse
-export type UserLinksStats = GetUserLinksStatsResponse
-export type UserLinksStatsResponse = GetUserLinksStatsResponse
